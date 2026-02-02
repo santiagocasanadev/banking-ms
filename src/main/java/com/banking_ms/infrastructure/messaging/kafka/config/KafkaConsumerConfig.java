5f951f3b336117d6d89e04e4546bdc2011deb1ee
Santiago Casana <santiagocasana@gmail.com>
2026-02-02 18:45:23 -0500

    Refactor yml files

--- src/main/java/com/banking_ms/infrastructure/messaging/kafka/config/KafkaConsumerConfig.java
+++ src/main/java/com/banking_ms/infrastructure/messaging/kafka/config/KafkaConsumerConfig.java
@@ -1,10 +1,16 @@
 package com.banking_ms.infrastructure.messaging.kafka.config;
 
 import com.banking_ms.infrastructure.messaging.kafka.dto.TransactionCreatedEvent;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
+@ConditionalOnProperty(
+        value = "kafka.enabled",
+        havingValue = "true",
+        matchIfMissing = false
+)
 public class KafkaConsumerConfig {
 
     @KafkaListener(topics = "transaction.created", groupId = "banking-ms")
