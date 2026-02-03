19641b674ab26d5a886a83ff5bbc7902d790d6e3
Santiago Casana <santiagocasana@gmail.com>
2026-02-02 19:12:09 -0500

    Repository bean added

--- src/main/java/com/banking_ms/infrastructure/adapter/out/persistence/repository/JpaAccountRepository.java
+++ src/main/java/com/banking_ms/infrastructure/adapter/out/persistence/repository/JpaAccountRepository.java
@@ -2,8 +2,10 @@ package com.banking_ms.infrastructure.adapter.out.persistence.repository;
 
 import com.banking_ms.infrastructure.adapter.out.persistence.entity.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface JpaAccountRepository extends JpaRepository<AccountEntity, UUID> {
 }
--- src/main/java/com/banking_ms/infrastructure/adapter/out/persistence/repository/JpaTransactionRepository.java
+++ src/main/java/com/banking_ms/infrastructure/adapter/out/persistence/repository/JpaTransactionRepository.java
@@ -2,8 +2,10 @@ package com.banking_ms.infrastructure.adapter.out.persistence.repository;
 
 import com.banking_ms.infrastructure.adapter.out.persistence.entity.TransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface JpaTransactionRepository extends JpaRepository<TransactionEntity, UUID> {
 }
